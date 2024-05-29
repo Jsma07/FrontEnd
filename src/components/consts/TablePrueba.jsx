ee0adb292de206abe503c1225f59a12fbdcb126e
emersonvr7 <emersonvargasquintero9@gmail.com>
2024-05-29 13:11:26 -0500

    TablaPrueba

--- src/components/consts/TablePrueba.jsx
+++ src/components/consts/TablePrueba.jsx
@@ -29,7 +29,7 @@ const TablePrueba = ({ columns, data }) => {
         <div className="relative overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
           <div className="flex flex-col px-4 py-3 space-y-3 lg:flex-row lg:items-center lg:justify-between lg:space-y-0 lg:space-x-4">
             <div className="flex items-center flex-1 space-x-4">
-               <h5>
+            <h5>
                 <span className="text-gray-500">All Products:</span>
                 <span className="dark:text-white">123456</span>
               </h5>
