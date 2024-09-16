83cb1cdde17dcf0203a74155b94762abdb96bb86
Eduardox03 <166255450+Eduardox03@users.noreply.github.com>
2024-09-16 16:16:01 -0500

    Update modalContrasena.jsx

--- src/components/consts/modalContrasena.jsx
+++ src/components/consts/modalContrasena.jsx
@@ -15,101 +15,167 @@ const ModalContrasena = ({ open, handleClose, handleSubmit }) => {
   };
 
   const handleChangeNewPassword = (e) => {
-    const value = e.target.value;
-    setNewPassword(value);
+    setNewPassword(e.target.value);
   };
 
   const handleChangeConfirmPassword = (e) => {
-    const value = e.target.value;
-    setConfirmPassword(value);
+    setConfirmPassword(e.target.value);
   };
 
   return (
-    <Modal open={open} onClose={handleClose}>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      style={{ backdropFilter: 'blur(8px)' }} // Mejora el desenfoque de fondo
+    >
       <div
         style={{
           position: "absolute",
           top: "50%",
           left: "50%",
           transform: "translate(-50%, -50%)",
+          backgroundColor: "white",
+          borderRadius: "20px",
+          padding: "24px",
+          minWidth: "300px",
+          maxWidth: "400px",
+          boxShadow: "0 12px 24px rgba(0,0,0,0.3)",
+          outline: "none", // Elimina el borde por defecto
         }}
       >
-        <div
-          style={{
-            backgroundColor: "white",
-            borderRadius: "8px",
-            padding: "20px",
-            minWidth: "300px",
-            maxWidth: "400px",
-          }}
+        <Typography
+          variant="h6"
+          gutterBottom
+          style={{ textAlign: "center", marginBottom: "2rem", fontWeight: 'bold' }}
         >
-          <Typography
-            variant="h5"
-            gutterBottom
-            style={{ textAlign: "center", marginBottom: "1.5rem" }}
-          >
-            <LockIcon
-              fontSize="large"
-              style={{ verticalAlign: "middle", marginRight: "8px" }}
-            />
-            Cambiar Contraseña
-          </Typography>
-          <form onSubmit={handleFormSubmit}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  id="newPassword"
-                  name="newPassword"
-                  label="Nueva Contraseña"
-                  variant="outlined"
-                  fullWidth
-                  type="password"
-                  value={newPassword}
-                  onChange={handleChangeNewPassword}
-                  required
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  id="confirmPassword"
-                  name="confirmPassword"
-                  label="Confirmar Contraseña"
-                  variant="outlined"
-                  fullWidth
-                  type="password"
-                  value={confirmPassword}
-                  onChange={handleChangeConfirmPassword}
-                  required
-                />
-              </Grid>
+          <LockIcon
+            fontSize="medium"
+            style={{ verticalAlign: "middle", marginRight: "8px" }}
+          />
+          Cambiar Contraseña
+        </Typography>
+        <form onSubmit={handleFormSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                id="newPassword"
+                name="newPassword"
+                label="Nueva Contraseña"
+                variant="outlined"
+                fullWidth
+                type="password"
+                value={newPassword}
+                onChange={handleChangeNewPassword}
+                required
+                InputProps={{
+                  style: {
+                    borderRadius: "8px",
+                    height: "50px", // Ajusta la altura del campo de entrada
+                    fontSize: "12px", // Tamaño del texto dentro del campo
+                    borderRadius: "20px",
+                  },
+                  inputProps: {
+                    style: {
+                      padding: "8px", // Ajusta el padding interno del campo
+                    }
+                  }
+                }}
+                InputLabelProps={{
+                  style: {
+                    fontSize: "12px", // Tamaño de la etiqueta
+                    top: "8px", // Ajusta la posición vertical de la etiqueta
+                  },
+                }}
+                style={{ marginBottom: "1.5rem" }} // Espacio entre campos
+              />
             </Grid>
-            <div
+            <Grid item xs={12}>
+              <TextField
+                id="confirmPassword"
+                name="confirmPassword"
+                label="Confirmar Contraseña"
+                variant="outlined"
+                fullWidth
+                type="password"
+                value={confirmPassword}
+                onChange={handleChangeConfirmPassword}
+                required
+                InputProps={{
+                  style: {
+                    borderRadius: "8px",
+                    height: "50px", // Ajusta la altura del campo de entrada
+                    fontSize: "12px", // Tamaño del texto dentro del campo
+                    borderRadius: "20px",
+                    textAlign: "center"
+                  },
+                  inputProps: {
+                    style: {
+                      padding: "8px", // Ajusta el padding interno del campo
+                    }
+                  }
+                }}
+                InputLabelProps={{
+                  style: {
+                    fontSize: "12px", // Tamaño de la etiqueta
+                    top: "8px", // Ajusta la posición vertical de la etiqueta
+                  },
+                }}
+              />
+            </Grid>
+          </Grid>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "1.5rem",
+            }}
+          >
+            <Button
+              variant="contained"
+              onClick={handleClose}
               style={{
-                display: "flex",
-                justifyContent: "center",
-                marginTop: "1.5rem",
+                width: "45%",
+                marginRight: "10px",
+                borderRadius: "20px",
+                textTransform: "none",
+                padding: "8px 12px", // Padding del botón
+                fontWeight: "bold",
+                fontSize: "14px", // Tamaño del texto del botón
+                backgroundColor: "#000000", // Color negro
+                color: "#FFFFFF",
+                boxShadow: "0 4px 8px rgba(0,0,0,0.2)",
+                '&:hover': {
+                  backgroundColor: "#333333", // Color negro oscuro al pasar el cursor
+                }
               }}
             >
-              <Button
-                color="secondary"
-                variant="contained"
-                onClick={handleClose}
-                style={{ width: "45%", marginRight: "10px" }}
-              >
-                Cancelar
-              </Button>
-              <Button
-                variant="contained"
-                color="primary"
-                type="submit"
-                style={{ width: "45%" }}
-                startIcon={<SendIcon />}
-              >
-                Enviar
-              </Button>
-            </div>
-          </form>
-        </div>
+              Cancelar
+            </Button>
+            <Button
+              variant="contained"
+              type="submit"
+              style={{
+                width: "45%",
+                borderRadius: "20px",
+                textTransform: "none",
+                padding: "8px 12px", // Padding del botón
+                fontWeight: "bold",
+                fontSize: "14px", // Tamaño del texto del botón
+                backgroundColor: "#6A1B9A", // Color morado
+                color: "#FFFFFF",
+                boxShadow: "0 4px 8px rgba(0,0,0,0.2)",
+                '&:hover': {
+                  backgroundColor: "#4A148C", // Color morado oscuro al pasar el cursor
+                }
+              }}
+              startIcon={<SendIcon />}
+            >
+              Enviar
+            </Button>
+          </div>
+        </form>
+
+
       </div>
     </Modal>
   );
